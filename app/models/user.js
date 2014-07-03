ffaf7d514b1ff0812d0ab3668154bf589a2aa5e2
CindyConway <cindy@cindyconwaysair.att.net>
2014-07-03 12:08:39 -0700

    got bear essencials working

---
+++ app/models/user.js
@@ -0,0 +1,10 @@
+ar mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var ChallengeSchema   = new Schema({
+  desc: String,
+  viewable: String,
+  userid: ObjectId
+});
+
+module.exports = mongoose.model('Challenge', ChallengeSchema);
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express    = require('express');    // call express
 var app        = express();         // define our app using express
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
-var Bear       = require('./app/models/bear');
+var Challenge       = require('./app/models/challenge');
 
 // connect to Mongo database
 mongoose.connect('localhost'); // connect to our database
@@ -36,75 +36,77 @@ router.get('/', function(req, res) {
 
 // more routes for our API will happen here
 
-// on routes that end in /bears
+// on routes that end in /challenges
 // ----------------------------------------------------
-router.route('/bears')
+router.route('/challenges')
 
-  // create a bear (accessed at POST http://localhost:8080/bears)
+  // create a challenge (accessed at POST http://localhost:8080/challenges)
   .post(function(req, res) {
 
-    var bear = new Bear();    // create a new instance of the Bear model
-    bear.name = req.body.name;  // set the bears name (comes from the request)
+    var challenge = new Challenge();    // create a new instance of the challenge model
+    challenge.desc = req.body.desc;  // set the challenges desc (comes from the request)
+    challenge.viewable = req.body.viewable; 
+    challenge.userid = req.body.userid;
 
-    bear.save(function(err) {
+    challenge.save(function(err) {
       if (err)
         res.send(err);
 
-      res.json({ message: 'Bear created!' });
+      res.json({ message: 'Challenge created!' });
     });
 
   })
 
-  // get all the bears (accessed at GET http://localhost:8080/api/bears)
+  // get all the challenges (accessed at GET http://localhost:8080/api/challenges)
   .get(function(req, res) {
-    Bear.find(function(err, bears) {
+    Challenge.find(function(err, challenges) {
       if (err)
         res.send(err);
 
-      res.json(bears);
+      res.json(challenges);
     });
   });
 
-// on routes that end in /bears/:bear_id
+// on routes that end in /challenges/:challenge_id
 // ----------------------------------------------------
-router.route('/bears/:bear_id')
+router.route('/challenges/:challenge_id')
 
-  // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+  // get the challenge with that id (accessed at GET http://localhost:8080/api/challenges/:challenge_id)
   .get(function(req, res) {
-    Bear.findById(req.params.bear_id, function(err, bear) {
+    Challenge.findById(req.params.challenge_id, function(err, challenge) {
       if (err)
         res.send(err);
-      res.json(bear);
+      res.json(challenge);
     });
   })
 
-  // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
+  // update the challenge with this id (accessed at PUT http://localhost:8080/api/challenges/:challenge_id)
   .put(function(req, res) {
 
-    // use our bear model to find the bear we want
-    Bear.findById(req.params.bear_id, function(err, bear) {
+    // use our challenge model to find the challenge we want
+    Challenge.findById(req.params.challenge_id, function(err, challenge) {
 
       if (err)
         res.send(err);
 
-      bear.name = req.body.name;  // update the bears info
+      challenge.name = req.body.name;  // update the challenges info
 
-      // save the bear
-      bear.save(function(err) {
+      // save the challenge
+      challenge.save(function(err) {
         if (err)
           res.send(err);
 
-        res.json({ message: 'Bear updated!' });
+        res.json({ message: 'Challenge updated!' });
       });
 
     });
   })
 
-  // delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
+  // delete the challenge with this id (accessed at DELETE http://localhost:8080/api/challenges/:challenge_id)
   .delete(function(req, res) {
-    Bear.remove({
-      _id: req.params.bear_id
-    }, function(err, bear) {
+    Challenge.remove({
+      _id: req.params.challenge_id
+    }, function(err, challenge) {
       if (err)
         res.send(err);
 
